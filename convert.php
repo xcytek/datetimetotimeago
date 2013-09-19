097032eb1f0d4042effb8b423f02a0983002f245
Alejandro Acosta <alexcarter15@hotmail.com>
2013-09-19 16:55:06 -0700

    Fix $date_time variable

--- convert.php
+++ convert.php
@@ -3,7 +3,7 @@
 function convertDateTimetoTimeAgo($datetime){		
 
 	$date_time = str_replace("-", "", $datetime);
-	$date_time = str_replace(":", "", $datetime);
+	$date_time = str_replace(":", "", $date_time);
 
 	//Fecha estilo TimeAgo
 	return "<script>document.write(moment('$date_time', 'YYYYMMDDHHmm').fromNow());</script>";		
